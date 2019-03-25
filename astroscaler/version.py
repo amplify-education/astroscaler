587102e4ea052814d161bf5f3a4713220c5662bc
Johnathan Constance <jconstance@amplify.com>
2019-03-25 11:58:43 -0400

    version bump apparently?

--- astroscaler/version.py
+++ astroscaler/version.py
@@ -1,5 +1,5 @@
 """Place of record for the package version"""
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __rpm_version__ = "WILL_BE_SET_BY_RPM_BUILD"
 __git_hash__ = "WILL_BE_SET_BY_EGG_BUILD"
